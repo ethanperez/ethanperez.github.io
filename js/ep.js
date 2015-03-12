cec6b0325579266a9fe763180b65c124459f0d73
Ethan Perez <ethan@ethanperez.com>
2015-03-12 18:51:10 +0000

    Add Pictonic icons

--- js/ep.js
+++ js/ep.js
@@ -1,9 +1,6 @@
 $(function(){
   $(".hello").typed({
     strings: ["I'm Ethan^1500, and I enjoy ^600 programming", "I'm Ethan, and I enjoy ^400 solving problems", "I'm Ethan, and I enjoy ^900 building experiences", "I'm Ethan, and I enjoy ^3000 the simple things."],
-    typeSpeed: 60,
-    callback: function() {
-      $('.navbar-default').fadeIn("slow", "linear");
-    }
+    typeSpeed: 60
   });
 });
